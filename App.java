c3abe50fba38700e0ffd9358315b5061d460914b
mannumukhi <manasadende15@gmail.com>
2018-09-03 12:31:12 +0530

    maven_project adding into git

---
+++ App.java
@@ -0,0 +1,13 @@
+package inno_maven_prjct;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
